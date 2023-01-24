b05052993ce89371ca8858ec17a8081c41e4f65e
Andrey Simashev <andrey.simashev@yandex.com>
2023-01-24 23:24:34 +0300

    введите Ваше имя - Console_write

--- example_001_helloconsole/example_002_Hello_user/Program.cs
+++ example_001_helloconsole/example_002_Hello_user/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите Ваше имя ");
+﻿Console.Write("Введите Ваше имя ");
 string username = Console.ReadLine ();
 Console.WriteLine ("привет, ");
 Console.WriteLine (username);
