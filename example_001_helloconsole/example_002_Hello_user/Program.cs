3576d55923cf8bb04e60274f5371785e5cf61dee
Andrey Simashev <andrey.simashev@yandex.com>
2023-01-26 20:29:14 +0300

    izm

--- example_001_helloconsole/example_002_Hello_user/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Write("Введите Ваше имя ");
-string username = Console.ReadLine ();
-Console.WriteLine ("привет, ");
-Console.WriteLine (username);
--- example_003_sum/Program.cs
+++ example_003_sum/Program.cs
@@ -1,6 +1,5 @@
-﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
-Console.WriteLine (numberA);
+﻿int numberA = new Random().Next(4,10); // 1 2 3 4 ... 9
 int numberB = new Random().Next(1,10); // 1 2 3 4 ... 9
-Console.WriteLine (numberB);
+Console.WriteLine (Console.WriteLine(numberB); Console.WriteLine(numberA));
 int result = numberA + numberB;
 Console.WriteLine(result);
