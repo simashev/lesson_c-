ea1a719ecc2f069d828aa3779128a2e85d38e6c8
Andrey Simashev <andrey.simashev@yandex.com>
2023-01-31 00:06:30 +0300

    lectors

---
+++ lectors/example_003_sum/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = new Random().Next(4,10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine (Console.WriteLine(numberB); Console.WriteLine(numberA));
+int result = numberA + numberB;
+Console.WriteLine(result);
