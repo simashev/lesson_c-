73562d83dd168fa26e0d14b8f31f06d1ce2a0b9c
Andrey Simashev <andrey.simashev@yandex.com>
2023-02-03 23:50:02 +0300

    seminar3

--- example_002_Hello_user/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Write("Введите Ваше имя ");
-string username = Console.ReadLine ();
-Console.Write("привет, ");
-Console.Write(username);
---
+++ seminar/02_02_23/Task_21/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, 
+// которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 2D пространстве.
+
+// A (3,6); B(2, 1)-> 5,09
+// A(7, -5); B(1, -1)-> 7,21
+//sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
+// double s = Math.Sqrt(...);
+// мое решение:
+
+// Console.Clear();
+// Console.Write("Введите координаты точки ax = ");
+// int ax = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты точки ay = ");
+// int ay = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите координаты точки bx = ");
+// int bx = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты точки by = ");
+// int by = int.Parse(Console.ReadLine());
+
+// double s = (Math.Sqrt((ax - bx)*(ax - bx) + (ay - by)*(ay - by)));
+
+// Console.WriteLine($"Расстояние = {s}");
+
+// --
+Console.Clear();
+Console.Write("Введите координаты точки ax = ");
+int ax = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты точки ay = ");
+int ay = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты точки bx = ");
+int bx = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты точки by = ");
+int by = int.Parse(Console.ReadLine());
+
+double s = (Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2)));
+
+Console.WriteLine($"Расстояние = {s:f3}");
